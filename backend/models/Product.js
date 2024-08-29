290e59b83e902b1adddb6b342bf6525cc1f4cf1f
agkayster <ejike.chiboka@gmail.com>
2024-08-29 16:42:36 +0100

    Implement Register and Login route using hash, bcrypt, token and salt

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -8,9 +8,19 @@ const productSchema = new Schema(
 			type: String,
 			require: [true, 'please provide available amount'],
 		},
+
 		cost: { type: Number, require: [true, 'price must be provided'] },
+
 		productName: { type: String, require: [true, 'name must be provided'] },
+
 		createdAt: { type: Date, default: Date.now() },
+
+		// references the User Model //
+		createdBy: {
+			type: mongoose.Types.ObjectId,
+			ref: 'User', // ref means the schema we are referencing. to get the user who created a job, we take the schema from the User schema //
+			required: [true, 'Please provide a user'],
+		},
 	},
 	{ timestamps: true }
 );
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ import { login, register } from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.route('/login').post(login);
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
