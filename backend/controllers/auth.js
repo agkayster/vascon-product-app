be9f0d987858abb54f5d8261c598cdacd70e6766
agkayster <ejike.chiboka@gmail.com>
2024-08-29 15:15:06 +0100

    Install bcrypt, cors, express-rate-limit, joi, react-icons

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,20 +1,12 @@
 import { BadRequest } from '../errors/bad-request.js';
 import jwt from 'jsonwebtoken';
 
-const login = async (req, res) => {
-	// req.body because of login form
-	const { username, password } = req.body;
-
-	if (!username || !password) {
-		throw new BadRequest('Please provide a username and password', 400);
-	}
-
-	const id = new Date().getDate();
-	const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-		expiresIn: '30d',
-	});
-
-	res.status(200).json({ msg: 'user created', userToken: token });
+const register = async (req, res) => {
+	res.send('Register User');
 };
 
-export { login };
+const login = async (req, res) => {
+	res.send('Login User');
+};
+
+export { register, login };
